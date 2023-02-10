bb62ffcdfd89ae7d3dbba513e9ce6190ca55178d
Usuario <Usuario@CPD02-M17>
2023-02-10 16:13:03 -0300

    atualização

---
+++ AppBuscaCEP/AppBuscaCEP/Model/Endereco.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscaCEP.Model
+{
+    internal class Endereco
+    {
+    }
+}
---
+++ AppBuscaCEP/AppBuscaCEP/Service/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscaCEP.Service
+{
+    internal class DataService
+    {
+    }
+}

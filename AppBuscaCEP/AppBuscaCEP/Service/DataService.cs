47d7f51217a0248bc457d4b1261b2e40eda12e6f
kaaly06 <kaliany.palma@etec.sp.gov.br>
2023-02-28 15:25:50 -0300

    atualização

--- AppBuscaCEP/AppBuscaCEP/Service/DataService.cs
+++ AppBuscaCEP/AppBuscaCEP/Service/DataService.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace AppBuscaCEP.Service
 {
-    internal class DataService
+    public class DataService
+    {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/endereco/by-cep?cep=" + cep);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+
+            }
+
+            return end;
+        }
+
+
+        public static async Task<List<Bairro>> GetBairrosByIdCidade(int id_cidade)
+        {
+            List<Bairro> arr_bairros = new List<Bairro>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/bairro/by-cidade?cidade=" + id_cidade);
+                if (response.IsSuccessStatusCode)
     {
     }
 }
